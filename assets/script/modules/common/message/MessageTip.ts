7b3b737761a61c0b6afc9c29869e1fe01a83dd8d
cyk <935765353@qq.com>
2022-06-16 00:57:39 +0800

    项目初版

--- assets/script/modules/common/message/MessageTip.ts
+++ assets/script/modules/common/message/MessageTip.ts
@@ -10,10 +10,12 @@ export class MessageTip extends UIMsg {
     public static prefabUrl: string = 'prefab/common/MessageTip';
     @property({ type: Label })
     public lbl_msg: Label;
+    @property({ type: Label })
+    public lbl_msg1: Label;
     private dchg() {
         let self = this;
         let data = self.data;
-        self.lbl_msg.string = data.msg;
+        self.lbl_msg.string = self.lbl_msg1.string = data.msg;
         new Tween(this.node)
             .to(0.5, { position: new Vec3(0, 60, 0) }, { easing: 'elasticOut' })
             .delay(0.8)
