f0c8d3a09a8e4403c3d494e05bcffb4f74d09595
cyk <935765353@qq.com>
2023-03-13 00:05:42 +0800

    基础累修改

--- assets/script/framework/base/BaseUtil.ts
+++ assets/script/framework/base/BaseUtil.ts
@@ -3,7 +3,7 @@
  * @Author: CYK
  * @Date: 2022-05-19 11:39:05
  */
-import { Layers, Node, Scene, screen, Size, UIOpacity, UITransform, view } from "cc";
+import { Layers, Node, Scene, screen, Size, UIOpacity, UITransform, Vec2, view } from "cc";
 import { scaleMode } from "./ScaleMode";
 export namespace BaseUT {
     /** 获取宽度适配下屏幕的视图宽高*/
@@ -67,5 +67,57 @@ export namespace BaseUT {
     export function getFitY(min: number, max: number) {
         let windowSize = BaseUT.getLayerScaleSize();
         return min + (max - min) * (windowSize.height - 1068) / (1280 - 1068);
+    }
+
+      // 角度转弧度
+      export function angle_to_radian (angle: number): number {
+        // 角度转弧度公式
+        // π / 180 * 角度
+
+        // 计算出弧度
+        let radian = Math.PI / 180 * angle;
+        // 返回弧度
+        return(radian);
+    }
+
+
+    // 弧度转角度
+    export function radian_to_angle (radian: number): number {
+        // 弧度转角度公式
+        // 180 / π * 弧度
+
+        // 计算出角度
+        let angle = 180 / Math.PI * radian;
+        // 返回弧度
+        return(angle);
+    }
+
+
+    // 角度转向量   
+    export function angle_to_vector (angle: number): Vec2 {
+        // tan = sin / cos
+        // 将传入的角度转为弧度
+        let radian = this.angle_to_radian(angle);
+        // 算出cos,sin和tan
+        let cos = Math.cos(radian);// 邻边 / 斜边
+        let sin = Math.sin(radian);// 对边 / 斜边
+        let tan = sin / cos;// 对边 / 邻边
+        // 结合在一起并归一化
+        let vec = new Vec2(cos, sin).normalize();
+        // 返回向量
+        return(vec);
+    }
+
+
+    // 向量转角度
+    export function vector_to_angle (vector: Vec2): number {
+        // 将传入的向量归一化
+        let dir = vector.normalize();
+        // 计算出目标角度的弧度
+        let radian = dir.signAngle(new Vec2(1, 0));
+        // 把弧度计算成角度
+        let angle = -this.radian_to_angle(radian);
+        // 返回角度
+        return(angle);
     }
 }
